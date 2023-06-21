0050cfe6b80e66b2934b3b1081aa48220119e3d0
Paras Bansal <parasbansal685@gmail.com>
2023-06-21 15:00:39 +0530

    changes

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import { useSelector } from 'react-redux';
 import { selectUser } from '../features/userSlice';
-import axiosFetch from '../utils/axiosFetch';
 
 
 const Home = () => {
@@ -13,8 +12,13 @@ const Home = () => {
         const userHomePage = async () => {
             if (user) {
                 try {
-                    const res = await axiosFetch.get(`getdata`, {
-                    })
+                    const token = localStorage.getItem('Authorization');
+                    const res = await axios.get('https://server-express-pay-houy.vercel.app/getdata', {
+                        headers: {
+                            Authorization: `Bearer ${token}`,
+                        },
+                        withCredentials: true,
+                    });
                     setUserName(res.data.name);
                 } catch (err) {
                     console.log("eroor received")
--- src/utils/axiosFetch.js
+++ src/utils/axiosFetch.js
@@ -1,35 +1,15 @@
-import axios from 'axios';
+import axios from 'axios'
 
-const getAuthToken = () => {
-  console.log("Hellloooooo")
-  if (typeof window !== 'undefined') {
-    return localStorage.getItem('Authorization') || window.localStorage.getItem('Authorization');
-  }
-  return null;
-};
+let token =
+  (typeof window !== 'undefined') ? (localStorage.getItem("Authorization") ||
+    window.localStorage.getItem("Authorization")) : null
 
 const axiosFetch = axios.create({
-  baseURL: 'https://server-express-pay-houy.vercel.app/',
+  baseURL: "https://server-express-pay-houy.vercel.app/",
   headers: {
-    Authorization: `Bearer ${getAuthToken()}`,
+    Authorization: `Bearer ${token}`,
   },
   withCredentials: true,
 });
 
 export default axiosFetch;
-
-// import axios from 'axios'
-
-// let token =
-//   (typeof window !== 'undefined') ? (localStorage.getItem("Authorization") ||
-//     window.localStorage.getItem("Authorization")) : null
-
-// const axiosFetch = axios.create({
-//   baseURL: "https://server-express-pay-houy.vercel.app/",
-//   headers: {
-//     Authorization: `Bearer ${token}`,
-//   },
-//   withCredentials: true,
-// });
-
-// export default axiosFetch;
